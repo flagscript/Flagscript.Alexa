8203b8f2788cecb8d3f774bdca56fb4455f0a710
Greg Kaestle <flagscript@gmail.com>
2019-03-01 09:30:28 -0700

    Updating with Version request property implementation

--- src/Flagscript.Alexa/Request/AlexaRequest.cs
+++ src/Flagscript.Alexa/Request/AlexaRequest.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Newtonsoft.Json;
 
 namespace Flagscript.Alexa.Request
@@ -9,11 +11,38 @@ namespace Flagscript.Alexa.Request
     /// <remarks>
     /// Request documentation can be found at
     /// https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html.
-    /// This is coded as of 3.1.19.
+    /// This is coded as of date 3.1.19.
     /// </remarks>
     public class AlexaRequest
     {
-        
-    }
+
+		#region Properties 
+
+		/// <summary>
+		/// The version specifier for the Alexa Request.
+		/// </summary>
+		/// <value>Request version specifier.</value>
+		[JsonProperty("version")]
+        public string Version 
+        {
+            get
+			{
+				return "1.0";
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value) || value != "1.0")
+				{
+					throw new ArgumentOutOfRangeException(
+						"Value", 
+						"Flagscript.Alexa only supports Alexa requests with version 1.0."
+					);
+				}
+			}
+        }
+
+		#endregion
+
+	}
 
 }
