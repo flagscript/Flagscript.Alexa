390470c6bc86a502f89364b08ff04a228a528ea1
Greg Kaestle <flagscript@gmail.com>
2019-03-01 09:15:53 -0700

    Adding Alexa Request. Adding Newtonsoft.

---
+++ src/Flagscript.Alexa/Request/AlexaRequest.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace Flagscript.Alexa.Request
+{
+
+    /// <summary>
+    /// Base Request from Alexa.
+    /// </summary>
+    /// <remarks>
+    /// Request documentation can be found at
+    /// https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html.
+    /// This is coded as of 3.1.19.
+    /// </remarks>
+    public class AlexaRequest
+    {
+        
+    }
+
+}
