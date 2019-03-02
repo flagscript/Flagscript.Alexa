b6a85771772a562dd4e6da3a61c7d5b6aaa1fceb
Greg Kaestle <flagscript@gmail.com>
2019-03-01 21:09:40 -0700

    Unit Tests for Alexa Request object.

---
+++ test/Flagscript.Alexa.Test/Request/AlexaRequestTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Flagscript.Alexa.Request.Test
+{
+
+	/// <summary>
+	/// Unit Tests for <see cref="AlexaRequest"/>.
+	/// </summary>
+	public class AlexaRequestTest
+	{
+
+		/// <summary>
+		/// Tests the serialization of the version property will only accept and
+		/// return '1.0'.
+		/// </summary>
+		/// <param name="version">Version theory to test.</param>
+		[Theory]
+		[InlineData("1.0")]
+		[InlineData("1.2")]
+		public void TestVersionProperty(string version)
+		{
+
+			var requestJson = "{ \"version\": \"" + version + "\" }";
+			AlexaRequest alexaRequest = null;
+			try
+			{
+				alexaRequest = JsonConvert.DeserializeObject<AlexaRequest>(requestJson);
+				Assert.Equal("1.0", version);
+				Assert.Equal("1.0", alexaRequest.Version);
+			}
+			catch (JsonSerializationException)
+			{
+				Assert.NotEqual("1.0", version);
+			}
+		}
+
+
+	}
+
+}
