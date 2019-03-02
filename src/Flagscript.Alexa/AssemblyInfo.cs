08c81c04a63209606b10fb21bd895183f2a4d2c0
Greg Kaestle <flagscript@gmail.com>
2019-03-02 13:50:56 -0700

    Adding Lambda Serialization to Assembly

--- src/Flagscript.Alexa/AssemblyInfo.cs
+++ src/Flagscript.Alexa/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+// Enable Lambda Serializion
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+
+// Make internals visible to unit testing
 [assembly: InternalsVisibleTo("Flagscript.Alexa.Test")]
